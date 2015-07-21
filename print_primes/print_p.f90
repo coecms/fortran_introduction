07ffe87b89d97aa1b9963b9e7874367958df4dff
Holger Wolff <holger.wolff@monash.edu>
2015-07-21 12:25:01 +1000

    turned program into module use program

--- print_primes/print_p.f90
+++ print_primes/print_p.f90
@@ -1,4 +1,40 @@
-PROGRAM print_primes
+MODULE prime_mod
+  IMPLICIT NONE
+
+CONTAINS
+
+  function is_prime(candidate)
+    IMPLICIT NONE
+    integer, intent(in) :: candidate
+    logical :: is_prime
+    integer :: divisor
+
+    ! 2 or 3 won't be caught by the loop
+    if ( candidate == 2 .or. candidate == 3 ) then
+      is_prime = .TRUE.
+      RETURN
+    end if
+
+    is_prime = .FALSE.
+
+    ! 1 will result in a false positive, so catch it together
+    ! with 0 and all negative numbers.
+    if ( candidate < 2 ) RETURN
+
+    divisor = 3
+    DO 
+      if (mod(candidate, divisor) == 0) RETURN
+      divisor = divisor + 2
+      if (divisor**2 > candidate) EXIT
+    END DO
+    is_prime = .TRUE.
+    RETURN
+  end function is_prime
+
+END MODULE prime_mod
+
+PROGRAM print_p
+  USE prime_mod, only: is_prime
   IMPLICIT NONE
   INTEGER :: counter, divisor, candidate
 
@@ -15,19 +51,12 @@ PROGRAM print_primes
     ! Get a new candidate
     candidate = candidate+2
 
-    ! Initialise the inner loop
-    divisor = 3
-    inner_loop : DO 
-      if (mod(candidate, divisor) == 0) CYCLE outer_loop
-      divisor = divisor + 2
-      if (divisor**2 > candidate) EXIT inner_loop
-    END DO inner_loop
+    if (is_prime(candidate)) then
+      counter = counter + 1
+      print *, counter, candidate
 
-    ! Found one, increment the counter, and print it.
-    counter = counter + 1
-    print *, counter, candidate
-
-    ! If we have found enough, exit.
-    if (counter >= 50) EXIT outer_loop
+      ! If we have found enough, exit.
+      if (counter >= 50) EXIT outer_loop
+    end if
   END DO outer_loop
-END PROGRAM print_primes
+END PROGRAM print_p
